cc239bd6a2822e9d5efc7fdb2d53cca69d77bd10
Ch0cap1csou <kawisan94800@gmail.com>
2026-01-16 09:58:59 +0100

    feat: initialise le projet Node.js avec dépendances

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello depuis mon app Node 18 !");
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
